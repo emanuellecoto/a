67d43d134de52e514baf5f97987746d9ccb64b36
emanu <emanu@Emanuel>
2024-10-20 20:57:18 -0600

    header pt1

--- src/main/java/dent/dent/controller/IndexController.java
+++ src/main/java/dent/dent/controller/IndexController.java
@@ -4,10 +4,16 @@
  */
 package dent.dent.controller;
 
-/**
- *
- * @author baflo
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
-    
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+
+        return "index";
+    }
 }
