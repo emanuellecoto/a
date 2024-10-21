32a28f2e97712167c0b44da2c92001d353acc5e1
baflo <baflo@Arianna>
2024-10-20 19:55:18 -0600

    creacion de packages correccion

---
+++ src/main/java/dent/dent/domain/AlamcenDomain.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dent.dent.domain;
+
+/**
+ *
+ * @author baflo
+ */
+public class AlamcenDomain {
+    
+}
