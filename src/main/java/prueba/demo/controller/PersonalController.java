569a9441217fb9752ca81d88f5cb66ec7b4284fa
emanu <emanu@Emanuel>
2024-10-26 00:53:30 -0600

    empleados front

---
+++ src/main/java/prueba/demo/controller/PersonalController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/empleados")
+@Controller
+public class PersonalController {
+    
+    @GetMapping("personal")
+    public String empleados(){
+        return "/empleados/personal";
+    }
+}
